4d26e5e2d590a75b3b5180978644c186c6c91905
github-action-benchmark <github@users.noreply.github.com>
2026-02-03 11:17:17 +0000

    add Go Benchmarks (go) benchmark result for 84400cd7ba514859ce70b0d9d7bafe854b8bb7a5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770117351703,
+  "lastUpdate": 1770117437243,
   "repoUrl": "https://github.com/PoyrazK/thecloud",
   "entries": {
     "Go Benchmarks": [
@@ -61456,6 +61456,78 @@ window.BENCHMARK_DATA = {
             "value": 2,
             "unit": "allocs/op",
             "extra": "7743824 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "83272398+PoyrazK@users.noreply.github.com",
+            "name": "Hüseyin Poyraz Küçükarslan",
+            "username": "PoyrazK"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "84400cd7ba514859ce70b0d9d7bafe854b8bb7a5",
+          "message": "Merge pull request #47 from PoyrazK/feature/global-load-balancer\n\nfix(ci): add buildx setup to deploy jobs for cache support",
+          "timestamp": "2026-02-03T14:12:50+03:00",
+          "tree_id": "2168a1ab39f091173269684314c63d43583dec32",
+          "url": "https://github.com/PoyrazK/thecloud/commit/84400cd7ba514859ce70b0d9d7bafe854b8bb7a5"
+        },
+        "date": 1770117433971,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkInstanceServiceList",
+            "value": 2.114,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "566419156 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkInstanceServiceList - ns/op",
+            "value": 2.114,
+            "unit": "ns/op",
+            "extra": "566419156 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkInstanceServiceList - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "566419156 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkInstanceServiceList - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "566419156 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVPCServiceGet",
+            "value": 149.6,
+            "unit": "ns/op\t     208 B/op\t       2 allocs/op",
+            "extra": "8292441 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVPCServiceGet - ns/op",
+            "value": 149.6,
+            "unit": "ns/op",
+            "extra": "8292441 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVPCServiceGet - B/op",
+            "value": 208,
+            "unit": "B/op",
+            "extra": "8292441 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkVPCServiceGet - allocs/op",
+            "value": 2,
+            "unit": "allocs/op",
+            "extra": "8292441 times\n4 procs"
           }
         ]
       }
